a3580b2c85060847e5b0475f20b7668aad55f3ce
io-eightyeight <root@yourself.org>
2014-09-28 00:55:38 -0400

    Edit algorithm list

--- src/init.cpp
+++ src/init.cpp
@@ -238,7 +238,7 @@ std::string HelpMessage()
     strUsage += "  -loadblock=<file>      " + _("Imports blocks from external blk000??.dat file") + "\n";
     strUsage += "  -reindex               " + _("Rebuild block chain index from current blk000??.dat files") + "\n";
     strUsage += "  -par=<n>               " + _("Set the number of script verification threads (up to 16, 0 = auto, <0 = leave that many cores free, default: 0)") + "\n";
-    strUsage += "  -algo=<algo>           " + _("Mining algorithm: sha256d, scrypt, groestl, x11, blake") + "\n";
+    strUsage += "  -algo=<algo>           " + _("Mining algorithm: sha256d, x11, blake") + "\n";
     strUsage += "\n" + _("Block creation options:") + "\n";
     strUsage += "  -blockminsize=<n>      "   + _("Set minimum block size in bytes (default: 0)") + "\n";
     strUsage += "  -blockmaxsize=<n>      "   + _("Set maximum block size in bytes (default: 250000)") + "\n";
