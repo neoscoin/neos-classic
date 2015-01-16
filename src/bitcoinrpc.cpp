a7a7f4a7bbb41a097ca451a34613a51b601ccd6f
io-eightyeight <root@yourself.org>
2015-01-16 00:01:14 -0500

    backport: Add -rpckeepalive and disable RPC use of HTTP persistent connections.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1019,7 +1019,7 @@ void ServiceConnection(AcceptedConnection *conn)
             conn->stream() << HTTPReply(HTTP_UNAUTHORIZED, "", false) << std::flush;
             break;
         }
-        if (mapHeaders["connection"] == "close")
+        if ((mapHeaders["connection"] == "close") || (!GetBoolArg("-rpckeepalive", false)))
             fRun = false;
 
         JSONRequest jreq;
