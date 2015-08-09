844549db2fc5259c0d4213435badadea9da8c3ed
io-eightyeight <root@yourself.org>
2015-08-09 12:55:37 -0400

    checkpoint: block 91836

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -46,11 +46,12 @@ namespace Checkpoints
         ( 42778, uint256("0x000000000001e7d3ddebf7d90626cf750a6dc209550c1b163aa76220bb6de547"))
         ( 50480, uint256("0x000000000000015de2abe3c1e8a04954e5f69150180871eaecb248a98d9652c3"))
         ( 64973, uint256("0x0000000000000157d957d520fe343a6a11a1f1161cd892f239b695901bee2a32"))
+        ( 91836, uint256("0x000000000000012ca4bd6683bbe99a07d1fb815edeeaf14b1e44eeaf12be1081"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1425599426,  // * UNIX timestamp of last checkpoint block
-        160052,      // * total number of transactions between genesis and last checkpoint
+        1434065336,  // * UNIX timestamp of last checkpoint block
+        209366,      // * total number of transactions between genesis and last checkpoint
                      //   (the tx=... number in the SetBestChain debug.log lines)
         10000        // * estimated number of transactions per day after checkpoint
     };
